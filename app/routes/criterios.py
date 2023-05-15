26214dbd508da1aee08cceab10d7170921e356ee
victor.barros <victor.barros@pgmais.com.br>
2023-05-14 22:16:23 -0300

    Criando rotas criterios

--- app/routes/criterios.py
+++ app/routes/criterios.py
@@ -1,59 +1,23 @@
 import json
-from pydantic import BaseModel
 from fastapi import APIRouter, Request
 from fastapi.responses import JSONResponse
 
 routes_criterios = APIRouter()
 
-
-class Criterios(BaseModel):
-    email: str
-    password: str
-
-
-teste = {
-    "Nível Esforço": [{"criterio": "Período do Dia", "value": 7}],
-    "Período do Dia": [{"criterio": "Nível Esforço", "value": 5}],
-}
-
-teste2 = {
-    "Nível Esforço": [
-        {"criterio": "Período do Dia", "value": 7},
-        {"criterio": "Tempo Conclusão", "value": 2},
-        {"criterio": "Nível Experiência", "value": 5},
-        {"criterio": "Avaliação de Usuários", "value": 2},
-    ],
-    "Período do Dia": [
-        {"criterio": "Nível Esforço", "value": 2},
-        {"criterio": "Tempo Conclusão", "value": 2},
-        {"criterio": "Nível Experiência", "value": 2},
-        {"criterio": "Avaliação de Usuários", "value": 2},
-    ],
-    "Tempo Conclusão": [
-        {"criterio": "Nível Esforço", "value": 2},
-        {"criterio": "Período do Dia", "value": 5},
-        {"criterio": "Nível Experiência", "value": 2},
-        {"criterio": "Avaliação de Usuários", "value": 2},
-    ],
-    "Nível Experiência": [
-        {"criterio": "Nível Esforço", "value": 2},
-        {"criterio": "Período do Dia", "value": 5},
-        {"criterio": "Tempo Conclusão", "value": 3},
-        {"criterio": "Avaliação de Usuários", "value": 5},
-    ],
-    "Avaliação de Usuários": [
-        {"criterio": "Nível Esforço", "value": 2},
-        {"criterio": "Período do Dia", "value": 5},
-        {"criterio": "Tempo Conclusão", "value": 2},
-        {"criterio": "Nível Experiência", "value": 2},
-    ],
-}
+from app.models.criterios import Criterio, CriteriosModel
 
 
 @routes_criterios.post("/criterios")
-async def criterios(request: Request):
-    body = await request.body()
-    request = json.loads(body.decode("utf-8"))
+async def insert(criterio: Criterio):
+    criterio_model = CriteriosModel()
+    criterio_model.insert_criterio(criterio=criterio)
+    return JSONResponse(status_code=200, content={"success": True, "criterio": criterio.__dict__})
 
-    # @TODO -> Inserir toda a lógica de inserção no ArangoDB
-    return JSONResponse(status_code=200, content={"success": True, "request": request})
+
+@routes_criterios.delete("/criterios/{key}")
+async def delete(key: str):
+    criterio_model = CriteriosModel()
+    criterio_model.delete(key=key)
+    return JSONResponse(
+        status_code=200, content={"message": "Critério deletado com sucesso!"}
+    )
